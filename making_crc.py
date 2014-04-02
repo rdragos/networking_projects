94d68a63d94178fc1cdfb85ee7006983c1b8d467
Rotaru Dragos Alin <r.dragos0@gmail.com>
2014-04-03 01:03:26 +0300

    refactored crc

--- making_crc.py
+++ making_crc.py
@@ -2,11 +2,12 @@
 
 
 def BinToInt(x):
-    answer = int(x[0])
+    binary_integer = int(x[0])
     for idx_bit in range(1,len(x)):
-        answer <<= 1
-        answer |= int(x[idx_bit])
-    return answer
+        binary_integer <<= 1
+        binary_integer |= int(x[idx_bit])
+    return binary_integer
+
 class CRC32(object):
 
     " No magic here "
@@ -17,10 +18,11 @@ class CRC32(object):
         2: 1, 1: 1, 0: 1,
         }),
         dict({0:1},)
-        ]
+    ]
     def __init__(self, text, maxbit):
         sentence_in_binary = "".join(
-                bin(ord(letter))[2:] for letter in text)
+                    bin(ord(letter))[2:] for letter in text
+                )
 
         self.final_poly = 0
         for key in self.polynomial_coefficents[0]:
@@ -34,8 +36,11 @@ class CRC32(object):
             rolling_hash |= (1 << maxbit) * (int(sentence_in_binary[idx_bit]))
             rolling_hash ^= self.final_poly
 
-        print (bin(rolling_hash)[2:])
+        self.integrity = rolling_hash
+    def see_integrity(self):
+        return bin(self.integrity)[2:]
+
 def main():
     C = CRC32("Just a random text",32)
-
+    print (C.see_integrity())
 main()
